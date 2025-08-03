b61180134966d28960c893cc601797044ca3a146
khmurthy <khmurthy6@gmail.com>
2025-08-03 14:23:33 -0400

    Initial commit

---
+++ src/main/java/com/khmurthy/magicmirror/MagicMirrorClient.java
@@ -0,0 +1,10 @@
+package com.khmurthy.magicmirror;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicMirrorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
