3aa9a768ef5e683fa63de25c40e0f7d32431ad78
khmurthy <khmurthy6@gmail.com>
2025-08-04 11:46:27 -0400

    Fixed effects and made mirror save location manually

--- src/main/java/com/khmurthy/magicmirror/item/custom/MagicMirrorItem.java
+++ src/main/java/com/khmurthy/magicmirror/item/custom/MagicMirrorItem.java
@@ -1,6 +1,8 @@
 package com.khmurthy.magicmirror.item.custom;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,16 +11,20 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Objects;
 
 public class MagicMirrorItem extends Item {
+    private double posX = -1.0;
+    private double posY = -1.0;
+    private double posZ = -1.0;
+    private RegistryKey<World> dimension;
 
     public MagicMirrorItem(Settings settings) {
         super(settings);
@@ -28,6 +34,16 @@ public class MagicMirrorItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         // Begin using item
         user.setCurrentHand(hand);
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 3, 1));
+        //If user is sneaking, store a new position
+        if (user.isSneaking()) {
+            posX = user.getX();
+            posY = user.getY();
+            posZ = user.getZ();
+            dimension = user.getWorld().getRegistryKey();
+            world.playSoundFromEntity(user, SoundEvents.ENTITY_WARDEN_AMBIENT, SoundCategory.PLAYERS, 1.0f, 1.0f);
+        }
+
         return TypedActionResult.consume(user.getStackInHand(hand));
     }
 
@@ -38,30 +54,35 @@ public class MagicMirrorItem extends Item {
     }
 
     @Override
+    public SoundEvent getDrinkSound() {
+        return SoundEvents.ENTITY_WARDEN_HEARTBEAT;
+    }
+
+    @Override
     public UseAction getUseAction(ItemStack stack) {
-        // Animation to show while using (e.g., bow draw, block, eat)
-        return UseAction.BOW;
+        // Animation to show while using
+        return UseAction.DRINK;
     }
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         if (!world.isClient && user instanceof ServerPlayerEntity serverPlayer) {
-            BlockPos spawnPos = serverPlayer.getSpawnPointPosition();
-            RegistryKey<World> spawnDim = serverPlayer.getSpawnPointDimension();
 
             // Default to world spawn if player has no bed set
-            if (spawnPos == null) {
-                spawnPos = world.getSpawnPos();
-                spawnDim = World.OVERWORLD;
+            if (posY < 0.0) {
+                posX = world.getSpawnPos().getX();
+                posY = world.getSpawnPos().getY();
+                posZ = world.getSpawnPos().getZ();
+                dimension = World.OVERWORLD;
             }
 
-            ServerWorld targetWorld = Objects.requireNonNull(serverPlayer.getServer()).getWorld(spawnDim != null ? spawnDim : World.OVERWORLD);
+            ServerWorld targetWorld = Objects.requireNonNull(serverPlayer.getServer()).getWorld(dimension != null ? dimension : World.OVERWORLD);
 
             if (targetWorld != null) {
                 serverPlayer.teleport(targetWorld,
-                        spawnPos.getX() + 0.5,
-                        spawnPos.getY(),
-                        spawnPos.getZ() + 0.5,
+                        posX + 0.5,
+                        posY,
+                        posZ + 0.5,
                         serverPlayer.getYaw(),
                         serverPlayer.getPitch());
             }
@@ -71,14 +92,14 @@ public class MagicMirrorItem extends Item {
             //    stack.damage(1, user, user.);
             //}
 
-            // Play portal effect
+            // Play visual effect
             ServerWorld serverWorld = serverPlayer.getServerWorld();
             serverWorld.spawnParticles(ParticleTypes.SCULK_SOUL,
                     serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ(),
                     30, 0.3, 0.1, 0.3, 0.0);
         }
 
-        // Play portal sound
+        // Play sound
         world.playSoundFromEntity(user, SoundEvents.BLOCK_SCULK_CHARGE, SoundCategory.PLAYERS, 1.0f, 1.0f);
 
         return super.finishUsing(stack, world, user);
